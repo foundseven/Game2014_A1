f3ebb299dbbf1bfd230b7873e2764ebbe2dd8111
Emma <pawful@bell.net>
2024-10-17 15:58:47 -0400

    added sound and music managers, also added a home button

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static SoundManager instance;
+
+    AudioSource _audioSource;
+
+    void Awake()
     {
-        
+        if (instance != null && this != instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        _audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayAudioClip(AudioClip clip, float volume = 1f)
     {
-        
+        _audioSource.PlayOneShot(clip, volume);
     }
 }
